6e13f2b3cfd1fdf4cf8ca65dcff162024568f2d3
fl4wless <noah.bauditz@live.de>
2023-08-02 14:10:00 +0200

    feat: adds user join date to profile page

--- likes/templatetags/custom_tags.py
+++ likes/templatetags/custom_tags.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+
 from django import template
 
 register = template.Library()
@@ -6,3 +9,8 @@ register = template.Library()
 @register.filter
 def user_liked_post(post, user_id):
     return post.like_set.filter(author=user_id).exists()
+
+
+@register.filter
+def formatted_join_date(test, profile):
+    return profile.user.date_joined.strftime('%d.%m.%Y')
