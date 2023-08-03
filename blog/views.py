5ebfdefe497e255962b580f87ae200179c969cfe
fl4wless <noah.bauditz@live.de>
2023-08-03 17:29:20 +0200

    chore: removes unused import

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
