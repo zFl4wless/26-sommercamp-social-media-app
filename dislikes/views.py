6284b1afde4f74821da94d85efe50301952e4dd5
zfl4wless <noah.bauditz@live.de>
2023-08-03 10:46:19 +0200

    style/feat: some

---
+++ dislikes/views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+from blog.models import Post
+from dislikes.models import Dislike
+
+
+def dislike_post(request):
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        user_id = request.POST.get('user_id')
+
+        try:
+            post = Post.objects.get(pk=post_id)
+        except Post.DoesNotExist:
+            return HttpResponse("Invalid post_id. Post does not exist.")
+
+        try:
+            user = User.objects.get(pk=user_id)
+        except Post.DoesNotExist:
+            return HttpResponse("Invalid user_id. User does not exist.")
+
+        user_disliked = Dislike.objects.filter(post=post, author=user)
+        if user_disliked.exists():
+            user_disliked.delete()
+            return redirect(request.META['HTTP_REFERER'])
+
+        Dislike.objects.create(post=post, author=user)
+
+        return redirect(request.META['HTTP_REFERER'])
